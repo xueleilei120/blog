7719516725353464ef6bcae8c3d08674d8fa70d1
xueleilei120 <290484002@qq.com>
2017-04-17 23:23:53 +0800

    X修改布局
    修改布局

--- blog/settings.py
+++ blog/settings.py
@@ -87,16 +87,25 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'blog',
+#         'USER': 'root',
+#         'PASSWORD': 'pass',
+#         'HOST': '127.0.0.1',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog',
-        'USER': 'root',
-        'PASSWORD': 'pass',
-        'HOST': '127.0.0.1',
+        'NAME': 'xionger$record',
+        'USER': 'xionger',
+        'PASSWORD': '86816137chao',
+        'HOST': 'xionger.mysql.pythonanywhere-services.com',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -137,10 +146,10 @@ USE_TZ = False
 
 # 自己在本地使用过 collections 所以 static 会从record下的static查找，在换model_icon时必须替换record下static
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),  # tuble 中只有一个元素的时候必须加一个,
-)
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),  # tuble 中只有一个元素的时候必须加一个,
+# )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # 邮箱配置
 EMAIL_HOST = "smtp.sina.com"
